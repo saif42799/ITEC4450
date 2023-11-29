4d3390c0c158def45cbcab4e8d16a68a649c720a
saifshaikh <sshaikh4@ggc.edu>
2023-11-29 08:09:11 -0500

    mac changes

--- Chapter_16/login.php
+++ Chapter_16/login.php
@@ -11,27 +11,111 @@ if (isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
 // include config file
 require_once "config.php";
 
-$username = $password =  '';
+$username = $password = '';
 $username = $password_err = '';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
+    // check if username is empty
+    if (empty(trim($_POST['username']))) {
+        $username_err = 'Please enter username';
+    } else {
+        $username = trim($_POST['username']);
+    }
+
+    // check if password is empty
+    if (empty(trim($_POST['password']))) {
+        $password_err = 'Please enter password';
+    } else {
+        $password = trim($_POST['password']);
+    }
+
+    // validate credentials
+    if (empty($username_err) && empty($password_err)) {
+        // prepare a select statement
+        $sql = "SELECT id, username, password FROM users WHERE username = ?";
+
+        if ($stmt = mysqli_prepare($link, $sql)) {
+            // bind variables to the prepared swl statement as parameters
+            // parameter 's' stands for String, 'i' stands for integer and 'b' for blob
+            // we need to use 
+
+            mysqli_stmt_bind_param($stmt, 's', $param_username);
+
+            // set parameters
+            $param_username = $username;
+
+            // attempt to execute the prepared statement 
+            if (mysqli_stmt_execute($stmt)) {
+                // store result
+                mysqli_stmt_store_result($stmt);
+
+                // check if username exisits, if yes then verify password
+                if (mysqli_stmt_num_rows($stmt) > 1) {
+                    // bind results varibales
+                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+
+                    if (mysqli_stmt_fetch($stmt)) {
+                        if (password_verify($password, $hashed_password)) {
+                            // password id correct, so start a new session
+                            session_start();
+
+                            // store data in session varibales
+                            $_SESSION['loggedin'] = true;
+                            $_SESSION['id'] = $id;
+                            $_SESSION['username'] = $username;
+
+                            // redirect user to welcome page 
+                            header("Location: welcome.php");
+
+                        } else {
+                            // display an error message is password is not valid
+                            $password_err = "The password you entered was not valid";
+                        }
+
+                    }
+
+                } else {
+                    // display an error message is username doesn't exsist
+                    $username_err = "No account found with that username";
+                }
+
+            } else {
+                echo "Ooops! Something went wrong. Please try again later";
+            }
+
+        }
+        // close statement
+        mysqli_stmt_close($stmt);
+
+    }
+    // close connection
+    mysqli_close($link);
 }
 
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <style>
-        body { font: 14px sans-serif; text-align: center; }
-        .wrapper { width: 350px; padding: 20px; }
+        body {
+            font: 14px sans-serif;
+            text-align: center;
+        }
+
+        .wrapper {
+            width: 350px;
+            padding: 20px;
+        }
     </style>
 </head>
+
 <body class="w3-light-grey">
     <div class="wrapper w3-sand w3-border">
         <h2>Login</h2>
@@ -40,17 +124,21 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
         <div class="w3-container <?php echo (!empty($username_err)) ? 'has-error' : ''; ?> ">
             <label>Username</label>
-            <input type="text" class="w3-input" name="username" value="<?php echo 
-            $username; ?>">
-            <span class="w3-red"><?php echo $username_err ; ?></span>
-        
+            <input type="text" class="w3-input" name="username" value="<?php echo
+                $username; ?>">
+            <span class="w3-red">
+                <?php echo $username_err; ?>
+            </span>
+
         </div>
 
-        <div class="w3-container <?php echo (!empty($username_err)) ? 'has-error' : ''; ?> ">
+        <div class="w3-container <?php echo (!empty($password_err)) ? 'has-error' : ''; ?> ">
             <label>Password</label>
-            <input type="text" class="w3-input" name="password">
-            <span class="w3-red"><?php echo $password_err ; ?></span>
-     
+            <input type="password" class="w3-input" name="password">
+            <span class="w3-red">
+                <?php echo $password_err; ?>
+            </span>
+
         </div>
 
         <div class="w3-container">
@@ -63,6 +151,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <a href="reset-password.php" class="btn btn-warning">Reset your password</a>
         <a href="logout.php" class="btn btn-danger">Sign out of your account</a>
     </p>
-    
+
 </body>
+
 </html>
